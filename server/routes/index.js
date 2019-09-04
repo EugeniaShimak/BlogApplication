f5c8ecb706361acc293ff60fcf387b77dc6814ac
Eugenia Shimak <greenlighty24@gmail.com>
2019-09-05 00:59:06 +0500

    Created form of registration and login.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json(JSON.stringify({data:'ответ'}));
 });
 
 module.exports = router;

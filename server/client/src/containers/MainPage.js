b66fe32942965de77410d446b4b800f5f032a9b2
Eugenia Shimak <greenlighty24@gmail.com>
2019-11-04 23:52:00 +0500

    client side

---
+++ server/client/src/containers/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage() {
+    return (
+        <div className="MainPage">
+           You're in MainPage
+        </div>
+    );
+}
+
+export default MainPage;

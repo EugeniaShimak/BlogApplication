d29277a4e2779eaa4bb9fde58ef989e02736e649
Eugenia Shimak <greenlighty24@gmail.com>
2019-11-04 23:40:37 +0500

    'message'

---
+++ server/routes/postRouter.js
@@ -0,0 +1,14 @@
+// var express = require('express');
+// var PostRouter = express.Router();
+//
+//
+// var PostController = require('./controllers/PostController');
+//
+// PostRouter.get('/', PostController.getAllPosts);
+// PostRouter.post('/', PostController.addPost);
+// PostRouter.get('/:userId', PostController.getPostsByUser);
+// PostRouter.get('/:postId', PostController.getPost);
+// PostRouter.put('/:postId', PostController.updatedPost);
+// PostRouter.delete('/:postId', PostController.deletePost);
+//
+// module.exports = PostRouter;
